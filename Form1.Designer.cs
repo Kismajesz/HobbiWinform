99a693c4abd0bcaffbbd47b0a359d5cac69ab825
Zalán Bölkény <Kismajesz@gmail.com>
2019-10-24 20:19:55 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace _7het
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.list = new System.Windows.Forms.ListBox();
+            this.Betoltes = new System.Windows.Forms.Button();
+            this.Mentes = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.lblHobbi = new System.Windows.Forms.Label();
+            this.lblNev = new System.Windows.Forms.Label();
+            this.lblSzulDatum = new System.Windows.Forms.Label();
+            this.lblNeme = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.rbtnNo = new System.Windows.Forms.RadioButton();
+            this.rbtnFerfi = new System.Windows.Forms.RadioButton();
+            this.txtNev = new System.Windows.Forms.TextBox();
+            this.txtSzulDatum = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Szöveges Fájl(.txt)|*.txt|Minden Fájl|*.*";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // list
+            // 
+            this.list.FormattingEnabled = true;
+            this.list.Location = new System.Drawing.Point(292, 155);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(129, 134);
+            this.list.TabIndex = 0;
+            this.list.SelectedIndexChanged += new System.EventHandler(this.List_SelectedIndexChanged);
+            // 
+            // Betoltes
+            // 
+            this.Betoltes.Location = new System.Drawing.Point(346, 400);
+            this.Betoltes.Name = "Betoltes";
+            this.Betoltes.Size = new System.Drawing.Size(75, 23);
+            this.Betoltes.TabIndex = 1;
+            this.Betoltes.Text = "Betöltés";
+            this.Betoltes.UseVisualStyleBackColor = true;
+            this.Betoltes.Click += new System.EventHandler(this.Betoltes_Click);
+            // 
+            // Mentes
+            // 
+            this.Mentes.Location = new System.Drawing.Point(87, 400);
+            this.Mentes.Name = "Mentes";
+            this.Mentes.Size = new System.Drawing.Size(75, 23);
+            this.Mentes.TabIndex = 2;
+            this.Mentes.Text = "Mentes";
+            this.Mentes.UseVisualStyleBackColor = true;
+            this.Mentes.Click += new System.EventHandler(this.Mentes_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(292, 295);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(129, 20);
+            this.txt1.TabIndex = 3;
+            this.txt1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt1_KeyDown);
+            // 
+            // lblHobbi
+            // 
+            this.lblHobbi.AutoSize = true;
+            this.lblHobbi.Location = new System.Drawing.Point(289, 132);
+            this.lblHobbi.Name = "lblHobbi";
+            this.lblHobbi.Size = new System.Drawing.Size(81, 13);
+            this.lblHobbi.TabIndex = 4;
+            this.lblHobbi.Text = "Kedvenc Hobbi";
+            // 
+            // lblNev
+            // 
+            this.lblNev.AutoSize = true;
+            this.lblNev.Location = new System.Drawing.Point(84, 132);
+            this.lblNev.Name = "lblNev";
+            this.lblNev.Size = new System.Drawing.Size(27, 13);
+            this.lblNev.TabIndex = 5;
+            this.lblNev.Text = "Név";
+            // 
+            // lblSzulDatum
+            // 
+            this.lblSzulDatum.AutoSize = true;
+            this.lblSzulDatum.Location = new System.Drawing.Point(84, 172);
+            this.lblSzulDatum.Name = "lblSzulDatum";
+            this.lblSzulDatum.Size = new System.Drawing.Size(61, 13);
+            this.lblSzulDatum.TabIndex = 6;
+            this.lblSzulDatum.Text = "Szül.Datum";
+            // 
+            // lblNeme
+            // 
+            this.lblNeme.AutoSize = true;
+            this.lblNeme.Location = new System.Drawing.Point(84, 211);
+            this.lblNeme.Name = "lblNeme";
+            this.lblNeme.Size = new System.Drawing.Size(32, 13);
+            this.lblNeme.TabIndex = 7;
+            this.lblNeme.Text = "Nem:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(223, 298);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Új hobbi:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(346, 331);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Hozzáad";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // rbtnNo
+            // 
+            this.rbtnNo.AutoSize = true;
+            this.rbtnNo.Location = new System.Drawing.Point(239, 209);
+            this.rbtnNo.Name = "rbtnNo";
+            this.rbtnNo.Size = new System.Drawing.Size(33, 17);
+            this.rbtnNo.TabIndex = 10;
+            this.rbtnNo.TabStop = true;
+            this.rbtnNo.Text = "N";
+            this.rbtnNo.UseVisualStyleBackColor = true;
+            // 
+            // rbtnFerfi
+            // 
+            this.rbtnFerfi.AutoSize = true;
+            this.rbtnFerfi.Location = new System.Drawing.Point(177, 209);
+            this.rbtnFerfi.Name = "rbtnFerfi";
+            this.rbtnFerfi.Size = new System.Drawing.Size(31, 17);
+            this.rbtnFerfi.TabIndex = 11;
+            this.rbtnFerfi.TabStop = true;
+            this.rbtnFerfi.Text = "F";
+            this.rbtnFerfi.UseVisualStyleBackColor = true;
+            // 
+            // txtNev
+            // 
+            this.txtNev.Location = new System.Drawing.Point(172, 132);
+            this.txtNev.Name = "txtNev";
+            this.txtNev.Size = new System.Drawing.Size(100, 20);
+            this.txtNev.TabIndex = 12;
+            // 
+            // txtSzulDatum
+            // 
+            this.txtSzulDatum.Location = new System.Drawing.Point(172, 172);
+            this.txtSzulDatum.Name = "txtSzulDatum";
+            this.txtSzulDatum.Size = new System.Drawing.Size(100, 20);
+            this.txtSzulDatum.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 450);
+            this.Controls.Add(this.txtSzulDatum);
+            this.Controls.Add(this.txtNev);
+            this.Controls.Add(this.rbtnFerfi);
+            this.Controls.Add(this.rbtnNo);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblNeme);
+            this.Controls.Add(this.lblSzulDatum);
+            this.Controls.Add(this.lblNev);
+            this.Controls.Add(this.lblHobbi);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.Mentes);
+            this.Controls.Add(this.Betoltes);
+            this.Controls.Add(this.list);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ListBox list;
+        private System.Windows.Forms.Button Betoltes;
+        private System.Windows.Forms.Button Mentes;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label lblHobbi;
+        private System.Windows.Forms.Label lblNev;
+        private System.Windows.Forms.Label lblSzulDatum;
+        private System.Windows.Forms.Label lblNeme;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.RadioButton rbtnNo;
+        private System.Windows.Forms.RadioButton rbtnFerfi;
+        private System.Windows.Forms.TextBox txtNev;
+        private System.Windows.Forms.TextBox txtSzulDatum;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace _7het
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.FileOk += (sender, e) =>
+                {
+                    try
+                    {
+                        using (var sr = new StreamReader(openFileDialog1.FileName))
+                        {
+                            list.Items.Clear();
+                            while (!sr.EndOfStream)
+                            {
+                                list.Items.Add(sr.ReadLine());
+                            }
+                        }
+                    }
+                    catch (IOException)
+                    {
+
+                        MessageBox.Show("Hiba! Nem sikerult a betoltes!");
+                    }
+                };
+        }
+
+        //enter-el való hozzáadás
+        private void txt1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && txt1.Text.Trim() !="")
+            {
+                list.Items.Add(txt1.Text);
+                txt1.Text = "";
+            }
+        }
+        //mentes
+        private void Mentes_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+
+            // csak szam lehet
+
+            //if (Int32.Parse(txtSzulDatum.Text.Trim()) != 0)
+            //{
+            //    MessageBox.Show("A születési dátum csak szám lehet");
+            //}
+
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                using (var sw = new StreamWriter(saveFileDialog1.FileName))
+                {
+                    //név fáljba írása
+
+                    foreach (var item in "Név: "+txtNev.Text+"\n")
+                    {
+                        sw.Write(item);
+                    }
+
+
+                    //datum fáljba írása
+
+
+                    if (Int32.Parse(txtSzulDatum.Text.Trim()) == 0)
+                    {
+                        foreach (var item in "Születési Dátuma: " + txtSzulDatum.Text + "\n")
+                        {
+                            sw.Write(item);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("A születési dátum csak szám lehet");
+                    }
+
+
+
+                    //ferfi faljba irasa
+
+                    if (rbtnFerfi.Checked == true)
+                    {
+
+                        foreach (var item in  rbtnFerfi.Text)
+                        {
+                            sw.WriteLine(item);
+                        }
+
+                    }
+
+                    //nő faljba irasa
+                    if (rbtnNo.Checked == true)
+                    {
+                        foreach (var item in rbtnNo.Text)
+                        {
+                        sw.WriteLine(item);
+                         }
+
+                    }
+
+                    //kivalasztott hobbi faljba irasa
+
+                    for (int i = 0; i < list.SelectedItems.Count; i++)
+                    {
+                        string text = list.GetItemText(list.SelectedItem);
+                        sw.WriteLine(text);
+                    }
+
+
+
+                }
+            }
+
+            
+
+            catch (IOException)
+            {
+
+                MessageBox.Show("Hiba, sikertelen mentés!");
+            }
+        }
+
+        //betoltes 
+        private void Betoltes_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+
+        //hozzáadás gomb (Ugyan az a hobbi nem lehet 2x-er)
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+
+            if (list.Items.Count == 0 && txt1.Text != "")
+            {
+                list.Items.Add(txt1.Text);
+                txt1.Text = "";
+            }
+            else
+            {
+                if (list.Items.Contains(txt1.Text) == false && txt1.Text != "")
+                {
+                    list.Items.Add(txt1.Text);
+                    txt1.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("Ugyan azt a hobbit nem adhatja meg.!");
+                }
+            }
+
+
+
+
+        }
+
+        private void List_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            
+        }
+    }
+}
